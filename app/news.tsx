ea38cefd99f49c47725a1459cabfc1352c8564ab
Chris Liang <christopherliang.cl@gmail.com>
2024-01-04 16:29:57 -0500

    Added country icons and clouds

--- app/news.tsx
+++ app/news.tsx
@@ -6,11 +6,12 @@ import {
   forwardRef,
 } from "react"
 import { Dialog, Transition } from '@headlessui/react'
+import { CircleFlag } from 'react-circle-flags'
 import dynamic from "next/dynamic"
 
 const News = (props: any) => {
   const cancelButtonRef = useRef(null)
-  const [articles, setArticles] = useState([])
+  const [articles, setArticles] = useState<any[]>([])
 
   useEffect(() => {
     if(props.country){
@@ -19,12 +20,12 @@ const News = (props: any) => {
             redirect: 'follow'
         };
 
-        // fetch(`https://api.worldnewsapi.com/search-news?source-countries=${props.country}&sort=publish-time&sort-direction=DESC&api-key=7a7ad3aa35344b389cca985f700d823a`, requestOptions)
+        // fetch(`https://api.worldnewsapi.com/search-news?source-countries=${props.country.ISO_A2}&sort=publish-time&sort-direction=DESC&api-key=7a7ad3aa35344b389cca985f700d823a`, requestOptions)
         // .then(response => response.text())
         // .then(result => console.log(result))
         // .catch(error => console.log('error', error));
 
-        fetch(`https://newsapi.org/v2/top-headlines?country=${props.country}&apiKey=747ba7350a824b9488fe577aab6a12d0`, requestOptions)
+        fetch(`https://newsapi.org/v2/top-headlines?country=${props.country.ISO_A2}&apiKey=747ba7350a824b9488fe577aab6a12d0`, requestOptions)
         .then(response => response.text())
         .then(result => setArticles(JSON.parse(result).articles))
         .catch(error => console.log('error', error));
@@ -33,7 +34,8 @@ const News = (props: any) => {
     }
   }, [props])
 
-  const cards = articles.map((article) =>
+  const cards = articles.map((article, i) =>
+    <>
     <div className="bg-white px-4 pb-4 py-3 sm:p-6 sm:pb-4">
     <div className="sm:flex sm:items-start">
     {/* <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
@@ -50,7 +52,35 @@ const News = (props: any) => {
     </div>
     </div>
     </div>
+    {i !== articles.length ? <hr/> : null}
+    </>
   )
+
+  const get_name = () => {
+    return (props.country) ? props.country.BRK_NAME : "this"
+  }
+
+  const get_country_code = () => {
+    return (props.country) ? props.country.ISO_A2.toLowerCase() : null
+  }
+
+  const no_cards =
+    <div className="bg-white px-4 py-3 sm:p-6 sm:py-4">
+    <div className="sm:flex sm:items-start">
+    {/* <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+    </div> */}
+    <div className="my-3 text-center sm:text-left">
+        <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
+            No news found, sorry!
+        </Dialog.Title>
+        <div className="mt-2">
+        <p className="text-sm text-gray-500">
+            Our current news API only supports N countries and {get_name()} is not one of them. Support may be added in future updates.
+        </p>
+        </div>
+    </div>
+    </div>
+    </div>
 
   return(
     <Transition.Root show={props.open} as={Fragment}>
@@ -79,7 +109,19 @@ const News = (props: any) => {
                 leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
                 <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                {cards.length > 0 ? cards : <>No News Found</>}
+                    <div className="bg-white px-4 py-3 sm:p-6 sm:py-4">
+                    <div className="sm:flex sm:items-center justify-center">
+                        <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                            <CircleFlag countryCode={get_country_code()} height="1"/>
+                        </div>
+                        <div className="sm:ml-3 sm:mt-0 mt-2 text-center sm:text-left">
+                        <Dialog.Title as="h2" className="text-base font-semibold leading-6 text-gray-900">
+                            {get_name()}
+                        </Dialog.Title>
+                        </div>
+                    </div>
+                    </div>
+                    {cards.length > 0 ? cards : no_cards}
                 </Dialog.Panel>
             </Transition.Child>
         </div>
--- app/world.tsx
+++ app/world.tsx
@@ -6,6 +6,7 @@ import {
   forwardRef,
 } from "react"
 import GlobeTmpl from "./globe"
+import * as THREE from 'three';
 
 const Globe = forwardRef((props: any, ref) => (
   <GlobeTmpl {...props} forwardRef={ref} />
@@ -28,12 +29,32 @@ const World = (props: any) => {
 
   useEffect(() => {
     // Auto-rotate
-    if (!globeRef.current) {
+    const globeEl = globeRef.current;
+
+    if (!globeEl) {
         return
     }
-    (globeRef.current as any).controls().autoRotateSpeed = 0.3;
+    (globeEl as any).controls().autoRotateSpeed = 0.3;
 
-    (globeRef.current as any).pointOfView({ altitude: 4 }, 5000);
+    (globeEl as any).pointOfView({ altitude: 4 }, 5000);
+
+    // Add clouds sphere
+    const CLOUDS_IMG_URL = 'https://unpkg.com/three-globe@2.30.0/example/clouds/clouds.png'; // from https://github.com/turban/webgl-earth
+    const CLOUDS_ALT = 0.005;
+    const CLOUDS_ROTATION_SPEED = 0; // deg/frame
+
+    new THREE.TextureLoader().load(CLOUDS_IMG_URL, cloudsTexture => {
+      const clouds = new THREE.Mesh(
+        new THREE.SphereGeometry((globeEl as any).getGlobeRadius() * (1 + CLOUDS_ALT), 75, 75),
+        new THREE.MeshPhongMaterial({ map: cloudsTexture, transparent: true })
+      );
+      (globeEl as any).scene().add(clouds);
+
+      (function rotateClouds() {
+        clouds.rotation.y += CLOUDS_ROTATION_SPEED * Math.PI / 180;
+        requestAnimationFrame(rotateClouds);
+      })();
+    });
   }, [globeReady]);
 
   useEffect(() => {
@@ -45,18 +66,22 @@ const World = (props: any) => {
     console.log(polygon.properties);
     (globeRef.current as any).controls().autoRotate = false;
     props.setOpen(true);
-    props.setCountry(polygon.properties.ISO_A2)
+    props.setCountry(polygon.properties)
   }
 
   return (
     <>
       <Globe
         globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
-        showAtmosphere={false}
+        bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+        showAtmosphere={true}
         polygonsData={countries.features}
-        polygonCapColor={(feature: any) => Math.max(0.1, Math.floor(Math.sqrt(+feature.properties.POP_EST/1.4e9)*255))}
+        // polygonCapColor={(feature: any) => Math.max(0.1, Math.floor(Math.sqrt(+feature.properties.POP_EST/1.4e9)*255))}
         polygonSideColor="#ffffff"
-        onPolygonClick={(polygon: any, event: any, {lat, lng, altitude}: any) => getCountry(polygon)}
+        polygonCapMaterial={{opacity: 0}}
+        // polygonSideMaterial={{opacity: 0}}
+        polygonAltitude={0.003}
+        onPolygonClick={(polygon: any) => getCountry(polygon)}
         onGlobeReady={() => setGlobeReady(true)}
         animateIn={false}
         ref={globeRef}
